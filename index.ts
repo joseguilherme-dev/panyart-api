b40a650eefec59b8009712fa571776214e0eb539
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-20 17:05:21 -0300

    :bulb: Add index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+import userAuthRouter from './routers/userAuthRouter';
 
 const app: Express = express();
+
 const port = 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+// Config
+app.use(express.json());
+
+// Endpoint raiz
+app.get('/', (req, res) => {
+  res.send('Bem-vindo!')
+})
+
+// Routes
+app.use('/auth', userAuthRouter)
+
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
