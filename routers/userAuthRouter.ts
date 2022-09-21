379cf92e58c7b681d5a02982484784fd8f6a600c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-21 13:11:49 -0300

    :arrow_up: Finish register

--- routers/userAuthRouter.ts
+++ routers/userAuthRouter.ts
@@ -3,41 +3,61 @@ import express, { Request, Response } from 'express';
 
 import { body, validationResult } from 'express-validator';
 
-import validateEmail from '../utils/validators/userAuthValidators';
+import {
+    validateEmail,
+    validatePassword,
+    validatePasswords,
+    validateNickname,
+    validateSocialMediaNickname
+} from '../utils/validators/userAuthValidators';
 
 
 const userRouter = express.Router()
 const prisma = new PrismaClient()
 
 
-
-
 userRouter.get('/login', (req: Request, res: Response) => {
     res.send('This is route: login');
 });
+
 
 userRouter.post(
     '/register',
     // Validations
     body('email').custom(validateEmail),
+    body('password1').custom(validatePassword),
+    body('password2').custom((password2, {req}) => {return validatePasswords(req.body.password1, password2)}),
+    body('nickname').custom(validateNickname),
+    body('twitter').custom(validateSocialMediaNickname),
+    body('discord').custom(validateSocialMediaNickname),
+    body('instagram').custom(validateSocialMediaNickname),
+    body('facebook').custom(validateSocialMediaNickname),
     async (req: Request, res: Response) => {
-    
+
     const body: any = req.body;
     const errors = validationResult(req);
 
     if (!errors.isEmpty())
         return res.status(400).json({errors: errors.array()});
 
-    const user = await prisma.user.create({
-        data: {
-            email: body.email,
-            nickname: body.nickname,
-            password: body.password,
-            coins: 0,
-        },
-    })
+    try {
+        await prisma.user.create({
+            data: {
+                email: body.email,
+                nickname: body.nickname,
+                password: body.password1,
+                coins: 0,
+                twitter: body.twitter,
+                facebook: body.facebook,
+                discord: body.discord,
+                instagram: body.instagram,
+            },
+        })
+    } catch (err) {
+        return res.status(500).json({serverError: 'Could not create user due to server error.'})
+    }
 
-    res.status(201).json({'msg': 'This is route: register',});
+    res.status(201).json({'success': 'The user was successfully created!',});
 });
 
 export default userRouter;
