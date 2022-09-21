8038256c69afb549c6c12962869c25099d0ae254
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-21 12:22:56 -0300

    :arrow_up: Add new migrations

---
+++ prisma/migrations/20220921135940_add_social_media_fields_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "discord" TEXT;
+ALTER TABLE "User" ADD COLUMN "facebook" TEXT;
+ALTER TABLE "User" ADD COLUMN "instagram" TEXT;
+ALTER TABLE "User" ADD COLUMN "twitter" TEXT;
