2a499e50c26c61d63405c362ef124b3e3c18830c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-22 13:21:46 -0300

    :bulb: Add admin router'

---
+++ routers/adminRouter.ts
@@ -0,0 +1,49 @@
+// Express
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+// Prisma
+import { PrismaClient } from '@prisma/client';
+
+// Utils
+import {
+    generateEncryptedPassword,
+    getUserIdByEmail,
+    isUserPasswordCorrect
+} from '../utils/auth';
+import { generateJWT } from '../utils/jwt';
+
+// Validators
+
+// Config
+import { ENV } from '../config';
+import testMiddleware from '../middlewares/testMiddleware';
+import { authenticatedOnlyMiddleware } from '../middlewares/authenticationMiddleware';
+import { staffOnlyMiddleware } from '../middlewares/staffMiddleware';
+import { generateCard } from '../utils/card';
+import { validateCardValue } from '../utils/validators/adminValidators';
+
+
+const adminRouter = express.Router()
+const prisma = new PrismaClient()
+
+
+adminRouter.post(
+    '/card',
+    // Field Validations,
+    body('value').custom(validateCardValue),
+    // Middlewares
+    authenticatedOnlyMiddleware,
+    staffOnlyMiddleware,
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+        const card: any = await generateCard(req.body.value)
+        return res.status(201).json({msg: 'Card successfully created!', card: {
+            code: card.code,
+            createdAt: card.createdAt,
+            coins: card.coins.toString()
+        }});
+});
+
+export default adminRouter;
