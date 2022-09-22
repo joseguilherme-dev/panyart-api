7e7bb03b265669134d3f513c5f9f6a1515cd2998
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-22 14:28:35 -0300

    :bulb: Add card utils

--- utils/card.ts
+++ utils/card.ts
@@ -14,3 +14,21 @@ export async function generateCard(value: number): Promise<object> {
     });
     return card
 }
+
+export async function isCardCodeValid(code: string){
+    const card: any = await prisma.card.findFirst({where: {code: code}})
+    if(!card)
+        return [false, "Inserted code does not exist!"]
+    if(card.redeemed)
+        return [false, "Inserted code has already been redeemed!"]
+    return [card, true]
+}
+
+export async function redeemCard(code: string, userId: string){
+    const card: any = await prisma.card.findFirst({where: {code: code}})
+    const user: any = await prisma.user.findFirst({where: {id: userId}, select: {coins: true},})
+    const newCoinsValue = card.coins + user.coins
+    await prisma.card.update({where: {code: card.code}, data: {redeemed: true, redeemedBy: userId, redeemedAt: new Date()}})
+    await prisma.user.update({where: {id: userId}, data: {coins: newCoinsValue}})
+    return card.coins
+}
