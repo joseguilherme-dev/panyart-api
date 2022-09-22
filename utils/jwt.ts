30ad80abc53bb991e3a11ff2b254a1e913658528
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-22 13:23:12 -0300

    :arrow_up: Update jwt utils

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -10,10 +10,3 @@ import {SECRET_KEY} from '../config';
 export function generateJWT(userId: string) {
     return jwt.sign({id: userId}, SECRET_KEY, {expiresIn: 3600});
 }
-
-// JWT Authentication Middleware
-export function authenticateJWT(req: Request, res: Response, next: NextFunction){
-    const JWT: any = req.headers['authorization']
-    jwt.verify(JWT, SECRET_KEY, (err: any) => {if (err) res.status(403).end()});
-    next();
-}
