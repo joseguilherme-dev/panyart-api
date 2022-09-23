2b4c7862bc7b2fdc45a4d7c7cf0d093ff600e3a6
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-23 15:45:44 -0300

    :bulb: Add email config

--- config.ts
+++ config.ts
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
+// Email
+export const DOMAIN: any = process.env.DOMAIN;
+export const NOREPLY_MAIL: any = process.env.NOREPLY_MAIL;
+export const NOREPLY_PASS: any = process.env.NOREPLY_PASS;
+
 export const ENV: any = process.env.ENV;
 export const SECRET_KEY: any = process.env.SECRET_KEY
 export const COINS_PER_DOLLAR = 5;
