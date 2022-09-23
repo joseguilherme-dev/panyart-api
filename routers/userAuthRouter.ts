3e1e093eb1b621b3eb7501a462cd252cd59b3061
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-23 12:08:49 -0300

    :bulb: Add password reset endpoints

--- routers/userAuthRouter.ts
+++ routers/userAuthRouter.ts
@@ -7,6 +7,7 @@ import { PrismaClient } from '@prisma/client';
 
 // Utils
 import {
+    changePassword,
     generateEncryptedPassword,
     getUserIdByEmail,
     isUserPasswordCorrect
@@ -27,7 +28,7 @@ import {
 // Config
 import { ENV } from '../config';
 import { authenticatedOnlyMiddleware } from '../middlewares/authenticationMiddleware';
-
+import { generateTOPT, verifyTOTP} from '../utils/otp';
 
 const userRouter = express.Router()
 const prisma = new PrismaClient()
@@ -68,7 +69,6 @@ userRouter.post(
     res.status(201).json({'success': 'The user was successfully created!',});
 });
 
-
 userRouter.post(
     '/login',
     // Field Validations
@@ -88,12 +88,49 @@ userRouter.post(
                   .status(200).json({success: 'The user has successfully logged in!', jwt: jwt});
 });
 
-
 userRouter.get(
     '/logout',
     authenticatedOnlyMiddleware,
     async (req: Request, res: Response) => {
         return res.clearCookie("jwt").status(200).json({success: 'The user has successfully logged out!'});
+});
+
+userRouter.get(
+    '/password/reset',
+    authenticatedOnlyMiddleware,
+    async (req: Request, res: Response) => {
+        const otp: string = generateTOPT(req.body.authenticatedId, 'pwdreset')
+        return res.status(200).json({otp: otp});
+});
+
+userRouter.get(
+    '/password/reset/verify',
+    // Field Validators,
+    body('token').isAlphanumeric(),
+    authenticatedOnlyMiddleware,
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+        return res.status(200).json({isOtpValid: verifyTOTP(req.body.token, req.body.authenticatedId, 'pwdreset')});
+});
+
+userRouter.post(
+    '/password/reset',
+    // Field Validators,
+    body('token').isAlphanumeric(),
+    body('password1').custom(validateSignUpPassword),
+    body('password2').custom((password2, {req}) => {return validateSignUpPasswords(req.body.password1, password2)}),
+    authenticatedOnlyMiddleware,
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+
+        const isOTPValid: boolean = verifyTOTP(req.body.token, req.body.authenticatedId, 'pwdreset')
+        if (!isOTPValid)
+            return res.status(401).json({msg: "Token is invalid or has expired."});
+
+        await changePassword(req.body.password1, req.body.authenticatedId)
+        return res.clearCookie("jwt").status(200).json({msg: "Password was succesfully reseted! Please, login again."});
 });
 
 
