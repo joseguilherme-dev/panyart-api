bf752779d680cab99d70ff33c23b710e0a6ddbe0
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-23 12:07:44 -0300

    :bulb: Add OTP utils

---
+++ utils/otp.ts
@@ -0,0 +1,28 @@
+import { SECRET_KEY } from "../config";
+import { authenticator } from 'otplib';
+
+/*
+ *  TOTP `type` are for maintaining different OTP for the same user depending on
+ *  what he is solicitating in our platform.
+ *
+ *  You should use these TOTP types:
+ *  -> 'pwdforgot', for password changing before login
+ *  -> 'pwdreset', for password changing after login
+ *  -> 'emailreset', for email changing after login
+ *
+ */
+
+export function generateTOPT(userId: string, type: string): string {
+    authenticator.options = {
+        digits: 6,
+        step: 600,
+        window: 0,
+    };
+    const totpSecret = SECRET_KEY + userId
+    return authenticator.generate(totpSecret);
+}
+
+export function verifyTOTP(token: string, userId: string, type: string): boolean {
+    const totpSecret = SECRET_KEY + userId
+    try { return authenticator.check(token, totpSecret)} catch (err) { return false }
+}
