f8ae110b7ba13483ceac13312b1539be4610564c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-23 09:59:22 -0300

    :bulb: Add Logout route

--- routers/userAuthRouter.ts
+++ routers/userAuthRouter.ts
@@ -26,31 +26,11 @@ import {
 
 // Config
 import { ENV } from '../config';
+import { authenticatedOnlyMiddleware } from '../middlewares/authenticationMiddleware';
 
 
 const userRouter = express.Router()
 const prisma = new PrismaClient()
-
-
-userRouter.post(
-    '/login',
-    // Field Validations
-    body('email').custom(validateLoginEmail),
-    body('password').custom(validateLoginPassword),
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
-
-        const isCorrect: any = await isUserPasswordCorrect(req.body.password, req.body.email)
-        if (!isCorrect)
-            return res.status(401).json({message: "Incorrect password!"})
-
-        const userId = await getUserIdByEmail(req.body.email)
-        const jwt: any = generateJWT(userId)
-
-        return res.cookie("jwt", jwt, {httpOnly: true, secure: (ENV === "PRODUCTION"),})
-                  .status(200).json({success: 'The user has successfully logged in!', jwt: jwt});
-});
 
 userRouter.post(
     '/register',
@@ -87,5 +67,35 @@ userRouter.post(
         }
     res.status(201).json({'success': 'The user was successfully created!',});
 });
+
+
+userRouter.post(
+    '/login',
+    // Field Validations
+    body('email').custom(validateLoginEmail),
+    body('password').custom(validateLoginPassword),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+
+        const isCorrect: any = await isUserPasswordCorrect(req.body.password, req.body.email)
+        if (!isCorrect) return res.status(401).json({message: "Incorrect password!"})
+
+        const userId = await getUserIdByEmail(req.body.email)
+        const jwt: any = generateJWT(userId)
+
+        return res.cookie("jwt", jwt, {httpOnly: true, secure: (ENV === "PRODUCTION"),})
+                  .status(200).json({success: 'The user has successfully logged in!', jwt: jwt});
+});
+
+
+userRouter.get(
+    '/logout',
+    authenticatedOnlyMiddleware,
+    async (req: Request, res: Response) => {
+        return res.clearCookie("jwt").status(200).json({success: 'The user has successfully logged out!'});
+});
+
+
 
 export default userRouter;
