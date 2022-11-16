1faef081bc895dfc9813df3cb6eca87bb8353ca4
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-16 10:58:18 -0300

    :wrench: Fixing bugs

--- index.ts
+++ index.ts
@@ -1,38 +1,37 @@
-import express, { Express, Request, Response } from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Routers
-import userAuthRouter from './routers/userAuthRouter';
-import adminRouter from './routers/adminRouter';
-import userRouter from './routers/userRouter';
+import userAuthRouter from "./routers/userAuthRouter";
+import adminRouter from "./routers/adminRouter";
+import userRouter from "./routers/userRouter";
 
 // Middlewares
-import { isAuthenticatedMiddleware } from './middlewares/authenticationMiddleware';
-import { isStaffMiddleware } from './middlewares/staffMiddleware';
+import { isAuthenticatedMiddleware } from "./middlewares/authenticationMiddleware";
+import { isStaffMiddleware } from "./middlewares/staffMiddleware";
 
 const app: Express = express();
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 // Config
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 
 // Endpoint raiz
-app.get('/', (req, res) => {
-  res.send('Bem-vindo!')
-})
+app.get("/", (req, res) => {
+  res.send("Bem-vindo!");
+});
 
 // Global Middlewares
-app.use(isAuthenticatedMiddleware)
+app.use(isAuthenticatedMiddleware);
 app.use(isStaffMiddleware);
 
 // Routers
-app.use('/auth', userAuthRouter)
-app.use('/admin', adminRouter)
-app.use('/user', userRouter)
-
+app.use("/auth", userAuthRouter);
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://0.0.0.0:${PORT}`);
--- middlewares/authenticationMiddleware.ts
+++ middlewares/authenticationMiddleware.ts
@@ -1,42 +1,56 @@
-import {
-    NextFunction,
-    Request,
-    Response
-} from 'express';
-import jwt from 'jsonwebtoken';
-import { SECRET_KEY } from '../config';
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config";
 
-export async function isAuthenticatedMiddleware(req: Request, res: Response, next: NextFunction){
-    /*
-     *      This middleware verifies if the user is authenticated using JWT stored in the cookies.
-     */
+export async function isAuthenticatedMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /*
+   *      This middleware verifies if the user is authenticated using JWT stored in the cookies.
+   */
 
-    // Reset information
-    req.body.isAuthenticated = false
-    req.body.authenticatedId = undefined
-    
-    if(!req.cookies.jwt){
-        req.body.isAuthenticated = false
-        return next()
+  // Reset information
+  req.body.isAuthenticated = false;
+  req.body.authenticatedId = undefined;
+
+  console.log(req.cookies);
+
+  if (!req.cookies.jwt) {
+    req.body.isAuthenticated = false;
+    return next();
+  } else {
+    const isJWTValid: any = await jwt.verify(
+      req.cookies.jwt,
+      SECRET_KEY,
+      (err: any) => {
+        if (err) return err;
+        else return jwt.decode(req.cookies.jwt, SECRET_KEY);
+      }
+    );
+    // If an error has not ocurred during the verification of JWT
+    // that means the JWT is valid, and the user is properly authenticated:
+    if (!isJWTValid.hasOwnProperty("name")) {
+      // Errors has 'name' property on it, containing the error name.
+      req.body.isAuthenticated = true;
+      req.body.authenticatedId = isJWTValid.id;
+      return next();
     } else {
-        const isJWTValid: any = await jwt.verify(req.cookies.jwt, SECRET_KEY, (err: any) => {
-            if (err) return err; else return jwt.decode(req.cookies.jwt, SECRET_KEY)
-        });
-        // If an error has not ocurred during the verification of JWT
-        // that means the JWT is valid, and the user is properly authenticated:
-        if (!isJWTValid.hasOwnProperty('name')){ // Errors has 'name' property on it, containing the error name.
-            req.body.isAuthenticated = true
-            req.body.authenticatedId = isJWTValid.id
-            return next()
-        } else {
-            // If the token is simply invalid (made up or has expired) the user is not authenticated.
-            res.clearCookie("jwt")
-            req.body.isAuthenticated = false
-            return next()
-        }
+      // If the token is simply invalid (made up or has expired) the user is not authenticated.
+      res.clearCookie("jwt");
+      req.body.isAuthenticated = false;
+      return next();
     }
+  }
 }
 
-export function authenticatedOnlyMiddleware(req: Request, res: Response, next: NextFunction){
-    if (!req.body.isAuthenticated) res.status(403).json({"msg": "Please login to execute this action."}).end(); else return next()
+export function authenticatedOnlyMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (!req.body.isAuthenticated)
+    res.status(403).json({ msg: "Please login to execute this action." }).end();
+  else return next();
 }
